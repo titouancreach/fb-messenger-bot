ba3281950e07b458adf325e62587d1107c5cb0ea
Titouan CREACH <titouan.creach@eegle.io>
2017-06-24 21:51:30 +0200

    test get request

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-print(sys.version_info[0])
-
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
+
+        print('GET REQUEST FROM FB')
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
