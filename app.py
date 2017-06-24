5b82712837404bb0061f394a8aa61b912451ff80
Titouan CREACH <titouan.creach@eegle.io>
2017-06-24 22:20:07 +0200

    I don't know what I'v done, but vim changed the endpoint url :'(

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def send_message(recipient_id, message_text):
             'text': message_text
         }
     })
-    r = requests.post('https://graph.facebook.com/v3.6/me/messages', params=params, headers=headers, data=data)
+    r = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
