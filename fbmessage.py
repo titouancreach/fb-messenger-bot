84d1dda05288453f961d4a583c1ef57bc029235b
Titouan CREACH <titouan.creach@eegle.io>
2017-06-24 22:26:02 +0200

    add type for event

--- fbmessage.py
+++ fbmessage.py
@@ -1,5 +1,5 @@
+import os
 import requests
-
 
 class FBMessagingEvent():
     """
@@ -10,6 +10,7 @@ class FBMessagingEvent():
 
     def __init__(self, rawevent):
         if rawevent.get('message'):
+            self.type = 'message'
             self.sender_id = rawevent['sender']['id']
             self.recipiend_id = rawevent['recipient']['id']
             self.msgtxt = rawevent['message']['text']
@@ -20,6 +21,9 @@ class FBMessagingEvent():
         Respond to the sender.
             :param msg Message to send back.
         """
+
+        if self.type != 'message':
+            return
 
         params = {
             'access_token': os.environ['PAGE_ACCESS_TOKEN']
