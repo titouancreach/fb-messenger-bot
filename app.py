0c601d7c69b00d3845151c117ac6d3ad57385106
Titouan CREACH <titouan.creach@eegle.io>
2017-06-24 22:14:42 +0200

    add class to parse message

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import sys
 import json
 
 import requests
+
+from fbmessage import FBMessagingEvent
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -61,7 +64,7 @@ def send_message(recipient_id, message_text):
             'text': message_text
         }
     })
-    r = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, headers=headers, data=data)
+    r = requests.post('https://graph.facebook.com/v3.6/me/messages', params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
---
+++ fbmessage.py
@@ -0,0 +1,44 @@
+import requests
+
+
+class FBMessagingEvent(Object):
+    """
+    Parse facebook messaging event and add posibilities to respond to the sender
+        :param rawevent The event sent by facebook webbhook.
+
+    """
+
+    def __init__(self, rawevent):
+        if rawevent.get('message'):
+            self.sender_id = rawevent['sender']['id']
+            self.recipiend_id = rawevent['recipient']['id']
+            self.msgtxt = rawevent['message']['text']
+
+
+    def respond(self, msg):
+        """
+        Respond to the sender.
+            :param msg Message to send back.
+        """
+
+        params = {
+            'access_token': os.environ['PAGE_ACCESS_TOKEN']
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        data = json.dumps({
+            'recipient': {
+                'id': self.recipiend_id
+            },
+            'message': {
+                'text': msg
+            }
+        })
+
+        r = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, headers=headers, data=data)
+        if r.status_code != 200:
+            print(r.status_code)
+            print(r.text)
+
