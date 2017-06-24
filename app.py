5872d8f184afba5052e7372e36d1ff7703936685
Titouan CREACH <titouan.creach@eegle.io>
2017-06-24 22:23:44 +0200

    test

--- app.py
+++ app.py
@@ -26,23 +26,10 @@ def webhook():
 
     if data['object'] == 'page':
         for entry in data['entry']:
-            for messaging_event in entry['messaging']:
-                if messaging_event.get('message'):
+            fbevents = [FBMessagingEvent(rawevent) for rawevent in entry['messaging']]
 
-                    sender_id = messaging_event['sender']['id']        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event['recipient']['id']  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event['message']['text']  # the message's text
-
-                    send_message(sender_id, 'Bonjour :)')
-
-                if messaging_event.get('delivery'):  # delivery confirmation
-                    pass
-
-                if messaging_event.get('optin'):  # optin confirmation
-                    pass
-
-                if messaging_event.get('postback'):  # user clicked/tapped 'postback' button in earlier message
-                    pass
+            for ev in fbevents:
+                ev.respond('MES COUILLES SUR TON NEZ')
 
     return 'ok', 200
 
